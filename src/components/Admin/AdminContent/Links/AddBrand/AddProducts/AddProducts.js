432f9b83919527a2a2968aa9753a951f867e124c
vadim <vadim.zubairov1993@gmail.com>
2020-10-29 19:16:57 +0500

    mod-admin

--- src/components/Admin/AdminContent/Links/AddBrand/AddProducts/AddProducts.js
+++ src/components/Admin/AdminContent/Links/AddBrand/AddProducts/AddProducts.js
@@ -19,7 +19,7 @@ function AddProducts(props) {
     setInputValue('');
     setInputImage('');
   }
-
+//// Получение
   React.useEffect(() => {
     axios.get('http://localhost:3002/products')
       .then(({data}) => {
@@ -27,7 +27,9 @@ function AddProducts(props) {
         setLists(result)
     })
   }, [setLists]);
+//// Получение
 
+//// Добавление
   const onAddList = (obj) => {
     const newList = [
       ...lists,
@@ -50,7 +52,9 @@ function AddProducts(props) {
         onClose()
     })
   }
+//// Добавление
 
+//// Удаление
   const removeList = (item) => {
     if(window.confirm('Вы точно хотите удалить?')) {
       axios.delete('http://localhost:3002/products/' + item.id).then(() => {
@@ -63,17 +67,134 @@ function AddProducts(props) {
     const newList = lists.filter(item => item.id !== id);
     setLists(newList)
   }
+//// Удаление
 
-  const onEditTitle = (id, title) => {
-    console.log(id, title);
+
+
+//// Изменение image
+  const onEditImage = (id, image) => {
+    const newImage = lists.map(item => {
+      if (item.id === id) {
+        item.image = image;
+      }
+      return item;
+    })
+    setLists(newImage)
   }
 
-  const editTitle = (item) => {
-    const newTitle = window.prompt('НАзввние', item.name);
-    if(newTitle) {
-      onEditTitle(item.id, newTitle)
+  const editImage = (item) => {
+    console.log(item.name);
+    const newImage = window.prompt('Внесите изменения', item.image);
+    if(newImage) {
+      onEditImage(item.id, newImage)
+      axios.patch('http://localhost:3002/products/' + item.id, {
+        image: newImage
+      }).catch(() => {
+        alert('ERROR')
+      })
     }
   }
+//// Изменение image
+
+//// Изменение name
+  const onEditName = (id, name) => {
+    const newName = lists.map(item => {
+      if (item.id === id) {
+        item.name = name;
+      }
+      return item;
+    })
+    setLists(newName)
+  }
+
+  const editName = (item) => {
+    console.log(item.name);
+    const newName = window.prompt('Внесите изменения', item.name);
+    if(newName) {
+      onEditName(item.id, newName)
+      axios.patch('http://localhost:3002/products/' + item.id, {
+        name: newName
+      }).catch(() => {
+        alert('ERROR')
+      })
+    }
+  }
+//// Изменение name
+
+//// Изменение color
+  const onEditColor = (id, color) => {
+    const newColor = lists.map(item => {
+      if (item.id === id) {
+        item.color = color;
+      }
+      return item;
+    })
+    setLists(newColor)
+  }
+
+  const editColor = (item) => {
+    console.log(item.name);
+    const newColor = window.prompt('Внесите изменения', item.color);
+    if(newColor) {
+      onEditColor(item.id, newColor)
+      axios.patch('http://localhost:3002/products/' + item.id, {
+        color: newColor
+      }).catch(() => {
+        alert('ERROR')
+      })
+    }
+  }
+//// Изменение color
+
+//// Изменение counter
+  const onEditCounter = (id, counter) => {
+    const newCounter = lists.map(item => {
+      if (item.id === id) {
+        item.counter = counter;
+      }
+      return item;
+    })
+    setLists(newCounter)
+  }
+
+  const editCounter = (item) => {
+    console.log(item.name);
+    const newCounter = window.prompt('Внесите изменения', item.counter);
+    if(newCounter) {
+      onEditCounter(item.id, newCounter)
+      axios.patch('http://localhost:3002/products/' + item.id, {
+        counter: newCounter
+      }).catch(() => {
+        alert('ERROR')
+      })
+    }
+  }
+//// Изменение counter
+
+//// Изменение description
+  const onEditDescr = (id, descr) => {
+    const newDescr = lists.map(item => {
+      if (item.id === id) {
+        item.description = descr;
+      }
+      return item;
+    })
+    setLists(newDescr)
+  }
+
+  const editDescr = (item) => {
+    console.log(item.name);
+    const newDescr = window.prompt('Внесите изменения', item.description);
+    if(newDescr) {
+      onEditDescr(item.id, newDescr)
+      axios.patch('http://localhost:3002/products/' + item.id, {
+        description: newDescr
+      }).catch(() => {
+        alert('ERROR')
+      })
+    }
+  }
+//// Изменение description
 
   return (
     <div>
@@ -83,10 +204,11 @@ function AddProducts(props) {
             lists.map((item, index) => {
               return (
                 <div key={index} className='products__item'>
-                  <p>{item.name}<button onClick={() => editTitle(item)}>ccc</button></p>
-                  <p>{item.color}</p>
-                  <p>{item.counter}</p>
-                  <p>{item.description}</p>
+                  <p>Image: {item.image}<i onClick={() => editImage(item)} className="fas fa-pen"></i></p>
+                  <p>{item.name}<i onClick={() => editName(item)} className="fas fa-pen"></i></p>
+                  <p>{item.color}<i onClick={() => editColor(item)} className="fas fa-pen"></i></p>
+                  <p>{item.counter}<i onClick={() => editCounter(item)} className="fas fa-pen"></i></p>
+                  <p>{item.description}<i onClick={() => editDescr(item)} className="fas fa-pen"></i></p>
                   <button 
                     onClick={() => removeList(item)}
                     className='products__item-close'>&times;
